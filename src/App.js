90af6f935eddc8a962657321f931096975a52899
Marco Brunetti <151136910+marcocodd@users.noreply.github.com>
2024-01-29 14:50:31 +0100

    finished

--- src/App.js
+++ src/App.js
@@ -10,15 +10,7 @@ function App() {
     <p>
      Edit <code>src/App.js</code> and save to reload.
     </p>
-    <a
-     className="App-link"
-     href="https://reactjs.org"
-     target="_blank"
-     rel="noopener noreferrer"
-    >
-     Learn React
-    </a>
-    <ButtonComponent text="Hello test" />
+    <ButtonComponent text="Hello test Button" />
     <ClassImgComponent
      url="http://placekitten.com/200/300"
      desc="kitten image"
